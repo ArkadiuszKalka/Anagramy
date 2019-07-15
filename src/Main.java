3ab1c218735bd8a3831459c052f36472fb3a4648
Arkadiusz Kałka <arkadiusz.kalka@gmail.com>
2019-07-15 12:21:28 +0200

    Dodano metode znajdzanagram

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
 
 public class Main {
 
@@ -7,10 +10,37 @@ public class Main {
 
     }
 
-    public boolean porownajSlowa(String str1, String str2){
+    public List<String> znajdzAnagram(List<String> listaSlow) {
+        Date dStart = new Date();
+        String s = "Rozpoczęto wyszukiwanie: ";
+        long totalComp = 0;
+        List<String> list = new ArrayList<>();
+        System.out.println(s);
 
-        String pStr1= str1.toLowerCase();
-        String pStr2= str2.toLowerCase();
+        for (int x = 0; x < listaSlow.size(); x++) {
+            for (int y = x + 1; y < listaSlow.size(); y++) {
+                String str1 = listaSlow.get(x).toString();
+                String str2 = listaSlow.get(y).toString();
+                totalComp++;
+                if (porownajSlowa(str1, str2)) {
+                    if ((x % 20) == 0) {
+                        System.out.println(".");
+                    }
+                list.add(str1+" - "+str2);
+                }
+
+
+            }
+
+        }
+
+    }
+
+
+    public boolean porownajSlowa(String str1, String str2) {
+
+        String pStr1 = str1.toLowerCase();
+        String pStr2 = str2.toLowerCase();
 
         char[] c1 = pStr1.toCharArray();
         char[] c2 = pStr2.toCharArray();
@@ -18,8 +48,8 @@ public class Main {
         Arrays.sort(c1);
         Arrays.sort(c2);
 
-        String sorted1= new String(c1);
-        String sorted2= new String(c2);
+        String sorted1 = new String(c1);
+        String sorted2 = new String(c2);
 
         return sorted1.equals(sorted2);
     }
